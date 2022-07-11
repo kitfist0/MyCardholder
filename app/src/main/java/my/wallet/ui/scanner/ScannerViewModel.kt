625991a75bb4a9e4617059cfb095e273ac82e577
kitfist0 <kitfist0@yandex.ru>
2022-07-11 23:02:13 +0300

    Initial commit

---
+++ app/src/main/java/my/wallet/ui/scanner/ScannerViewModel.kt
@@ -0,0 +1,13 @@
+package my.wallet.ui.scanner
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScannerViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Scanner Fragment"
+    }
+    val text: LiveData<String> = _text
+}
