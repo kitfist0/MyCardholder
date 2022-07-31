677936e546ff0e3dd6b50eec75a9a5c18e3e79a4
kitfist0 <kitfist0@yandex.ru>
2022-07-31 20:12:26 +0300

    Add card fragment

---
+++ app/src/main/java/my/cardholder/ui/card/CardFragment.kt
@@ -0,0 +1,35 @@
+package my.cardholder.ui.card
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import my.cardholder.databinding.FragmentCardBinding
+
+@AndroidEntryPoint
+class CardFragment : Fragment() {
+
+    private val viewModel: CardViewModel by viewModels()
+
+    private var _binding: FragmentCardBinding? = null
+
+    // This property is only valid between onCreateView and onDestroyView
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
