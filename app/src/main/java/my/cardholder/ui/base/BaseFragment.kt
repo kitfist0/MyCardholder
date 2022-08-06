4f7a731567d31c3a02ff38e3823c9f0ae0540f52
kitfist0 <kitfist0@yandex.ru>
2022-08-06 18:07:50 +0300

    Refactor base fragment

--- app/src/main/java/my/cardholder/ui/base/BaseFragment.kt
+++ app/src/main/java/my/cardholder/ui/base/BaseFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
@@ -19,18 +21,11 @@ abstract class BaseFragment<out VB : ViewBinding>(
 
     abstract fun initViews()
 
-    abstract suspend fun launchWhenStarted()
+    abstract fun collectData()
 
     private var _binding: VB? = null
 
     val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            viewModel.onBackPressed()
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,8 +44,14 @@ abstract class BaseFragment<out VB : ViewBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        collectData()
+    }
+
+    protected fun <T> Flow<T>.collectWhenStarted(action: (T) -> Unit) {
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            launchWhenStarted()
+            this@collectWhenStarted
+                .onEach { action.invoke(it) }
+                .collect()
         }
     }
 }
--- app/src/main/java/my/cardholder/ui/base/BaseViewModel.kt
+++ app/src/main/java/my/cardholder/ui/base/BaseViewModel.kt
@@ -3,6 +3,4 @@ package my.cardholder.ui.base
 import androidx.lifecycle.ViewModel
 
 abstract class BaseViewModel : ViewModel() {
-    open fun onBackPressed() {
-    }
 }
