ff387f68accaa5877ce74f8afefd357b7ca343a0
kitfist0 <kitfist0@yandex.ru>
2022-08-16 18:55:19 +0300

    Remove mock data from viewmodels

--- app/src/main/java/my/cardholder/ui/card/viewer/CardViewerViewModel.kt
+++ app/src/main/java/my/cardholder/ui/card/viewer/CardViewerViewModel.kt
@@ -1,31 +1,26 @@
 package my.cardholder.ui.card.viewer
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asFlow
+import androidx.lifecycle.liveData
 import androidx.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import my.cardholder.data.Card
+import my.cardholder.data.CardDao
 import my.cardholder.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CardViewerViewModel @Inject constructor(
+    cardDao: CardDao,
     savedStateHandle: SavedStateHandle,
 ) : BaseViewModel() {
 
     private val cardId = CardViewerFragmentArgs.fromSavedStateHandle(savedStateHandle).cardId
 
-    private val _card = MutableLiveData<Card>().apply {
-        value = Card(
-            id = cardId,
-            name = "Card title",
-            text = "b6589fc6ab0dc82cf12099d1c2d40ab994e8410c",
-            color = "",
-            format = "qr_code",
-            timestamp = 0L,
-        )
+    private val _card = liveData {
+        emit(cardDao.getCard(cardId))
     }
     val card: Flow<Card> = _card.asFlow()
 
--- app/src/main/java/my/cardholder/ui/cards/CardsViewModel.kt
+++ app/src/main/java/my/cardholder/ui/cards/CardsViewModel.kt
@@ -1,36 +1,22 @@
 package my.cardholder.ui.cards
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asFlow
+import androidx.lifecycle.liveData
 import androidx.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import my.cardholder.data.Card
+import my.cardholder.data.CardDao
 import my.cardholder.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CardsViewModel @Inject constructor(): BaseViewModel() {
+class CardsViewModel @Inject constructor(
+    cardDao: CardDao,
+): BaseViewModel() {
 
-    private val _cards = MutableLiveData<List<Card>>().apply {
-        value = listOf(
-            Card(
-                id = 0,
-                name = "Card0",
-                text = "b6589fc6ab0dc82cf12099d1c2d40ab994e8410c",
-                color = "",
-                format = "qr_code",
-                timestamp = 0L,
-            ),
-            Card(
-                id = 1,
-                name = "Card1",
-                text = "356a192b7913b04c54574d18c28d46e6395428ab",
-                color = "",
-                format = "qr_code",
-                timestamp = 0L,
-            ),
-        )
+    private val _cards = liveData {
+        emit(cardDao.getCards())
     }
     val cards: Flow<List<Card>> = _cards.asFlow()
 
