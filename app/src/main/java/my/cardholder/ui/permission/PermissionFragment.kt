d1106c858dae0383d0f59ab166e8f51902c67382
kitfist0 <kitfist0@yandex.ru>
2022-08-19 14:25:30 +0300

    Add logic for permission fragment and viewmodel

--- app/src/main/java/my/cardholder/ui/permission/PermissionFragment.kt
+++ app/src/main/java/my/cardholder/ui/permission/PermissionFragment.kt
@@ -1,5 +1,7 @@
 package my.cardholder.ui.permission
 
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import my.cardholder.databinding.FragmentPermissionBinding
@@ -10,6 +12,19 @@ class PermissionFragment : BaseFragment<FragmentPermissionBinding>(
     FragmentPermissionBinding::inflate
 ) {
 
+    private val permissionsRequestHandler = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        permissions.entries.forEach {
+            val shouldShow = ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), it.key)
+            viewModel.onPermissionRequestResult(
+                permission = it.key,
+                isGranted = it.value,
+                shouldShowRationale = shouldShow,
+            )
+        }
+    }
+
     override val viewModel: PermissionViewModel by viewModels()
 
     override fun initViews() {
@@ -19,5 +34,8 @@ class PermissionFragment : BaseFragment<FragmentPermissionBinding>(
     }
 
     override fun collectData() {
+        viewModel.requestPermissions.collectWhenStarted { permissions ->
+            permissionsRequestHandler.launch(permissions)
+        }
     }
 }
--- app/src/main/java/my/cardholder/ui/permission/PermissionViewModel.kt
+++ app/src/main/java/my/cardholder/ui/permission/PermissionViewModel.kt
@@ -1,12 +1,52 @@
 package my.cardholder.ui.permission
 
+import android.Manifest
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import my.cardholder.ui.base.BaseViewModel
+import my.cardholder.util.PermissionHelper
 import javax.inject.Inject
 
 @HiltViewModel
-class PermissionViewModel @Inject constructor() : BaseViewModel() {
+class PermissionViewModel @Inject constructor(
+    private val permissionHelper: PermissionHelper,
+) : BaseViewModel() {
+
+    companion object {
+        const val PERMISSION = Manifest.permission.CAMERA
+    }
+
+    private val _requestPermissions = MutableSharedFlow<Array<String>>()
+    val requestPermissions = _requestPermissions.asSharedFlow()
+
+    init {
+        if (permissionHelper.isPermissionGranted(PERMISSION)) {
+            navigate(PermissionFragmentDirections.fromPermissionToScanner())
+        }
+    }
+
     fun onGrantFabClicked() {
-        navigate(PermissionFragmentDirections.fromPermissionToScanner())
+        viewModelScope.launch {
+            _requestPermissions.emit(arrayOf(PERMISSION))
+        }
+    }
+
+    fun onPermissionRequestResult(
+        permission: String,
+        isGranted: Boolean,
+        shouldShowRationale: Boolean,
+    ) {
+        if (isGranted) {
+            navigate(PermissionFragmentDirections.fromPermissionToScanner())
+        } else {
+            permissionHelper.onPermissionIsNotGranted(
+                permission = permission,
+                shouldShowRationale = shouldShowRationale,
+                openAppInfoIfNeverAskChecked = true,
+            )
+        }
     }
 }
