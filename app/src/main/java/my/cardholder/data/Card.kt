1c66a1cdb44af3920aa34d4b3e9f86531e7310c9
kitfist0 <kitfist0@yandex.ru>
2022-09-22 22:05:28 +0300

    Add barcode file extension to card data model

--- app/src/main/java/my/cardholder/data/Card.kt
+++ app/src/main/java/my/cardholder/data/Card.kt
@@ -1,7 +1,9 @@
 package my.cardholder.data
 
+import android.content.Context
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.File
 
 @Entity(tableName = "cards")
 data class Card(
@@ -12,4 +14,10 @@ data class Card(
     val color: String,
     val timestamp: Long,
     val format: SupportedFormat,
-)
+) {
+    companion object {
+        fun Card.getBarcodeFile(context: Context): File {
+            return File(context.getExternalFilesDir("images"), "$timestamp.jpeg")
+        }
+    }
+}
