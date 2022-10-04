8bbde6af0ef845d360ba452a8c4d1d4cc964ba8e
kitfist0 <kitfist0@yandex.ru>
2022-10-04 21:20:25 +0300

    Set default night mode on button click

--- app/src/main/java/my/cardholder/ui/settings/SettingsFragment.kt
+++ app/src/main/java/my/cardholder/ui/settings/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package my.cardholder.ui.settings
 
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import my.cardholder.R
 import my.cardholder.databinding.FragmentSettingsBinding
 import my.cardholder.ui.base.BaseFragment
 
@@ -11,10 +14,23 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsB
     override val viewModel: SettingsViewModel by viewModels()
 
     override fun initViews() {
+        binding.settingsColorThemeButton.setOnClickListener {
+            viewModel.onColorThemeButtonClicked()
+        }
     }
 
     override fun collectData() {
-        viewModel.text.collectWhenStarted {
+        viewModel.setDefaultNightMode.collectWhenStarted { mode ->
+            AppCompatDelegate.setDefaultNightMode(mode)
+            binding.settingsColorThemeButton.apply {
+                if (mode == AppCompatDelegate.MODE_NIGHT_YES) {
+                    icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_light_mode)
+                    text = getString(R.string.settings_switch_to_light_mode)
+                } else {
+                    icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_dark_mode)
+                    text = getString(R.string.settings_switch_to_dark_mode)
+                }
+            }
         }
     }
 }
--- app/src/main/java/my/cardholder/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/my/cardholder/ui/settings/SettingsViewModel.kt
@@ -1,17 +1,29 @@
 package my.cardholder.ui.settings
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.asFlow
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.*
 import my.cardholder.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : BaseViewModel() {
+class SettingsViewModel @Inject constructor(
+    private val preferences: SharedPreferences,
+) : BaseViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Settings Fragment"
+    companion object {
+        private const val DARK_MODE_KEY = "dark_mode"
     }
-    val text: Flow<String> = _text.asFlow()
+
+    private val isDarkModeEnabled get() = preferences.getBoolean(DARK_MODE_KEY, false)
+
+    private val _setDefaultNightMode = MutableStateFlow(AppCompatDelegate.MODE_NIGHT_NO)
+    val setDefaultNightMode = _setDefaultNightMode.asStateFlow()
+
+    fun onColorThemeButtonClicked() {
+        val newValue = isDarkModeEnabled.not()
+        preferences.edit().putBoolean(DARK_MODE_KEY, newValue).apply()
+        _setDefaultNightMode.value = if (newValue) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+    }
 }
