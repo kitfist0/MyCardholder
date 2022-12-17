de3be6385aabb6e939503e7f1d7a0be01cc122a2
kitfist0 <kitfist0@yandex.ru>
2022-12-17 17:09:28 +0300

    Convert supported format to spec

--- app/src/main/java/my/cardholder/data/model/FormatSpec.kt
+++ app/src/main/java/my/cardholder/data/model/FormatSpec.kt
@@ -2,7 +2,15 @@ package my.cardholder.data.model
 
 data class FormatSpec(
     val name: String,
-    val image: String,
     val characters: String,
-    val length: String,
-)
+    val length: String = "unfixed",
+) {
+    companion object {
+        fun FormatSpec.getFileName(): String {
+            return name.lowercase()
+                .replace(" ", "")
+                .replace("-", "")
+                .plus(".jpg")
+        }
+    }
+}
