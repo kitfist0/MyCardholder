78175882cbdfc0bb9b80f40e818133fdefd13370
kitfist0 <kitfist0@yandex.ru>
2023-01-13 22:23:10 +0300

    Create search viewmodel and fragment logic

--- app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchFragment.kt
+++ app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchFragment.kt
@@ -1,10 +1,14 @@
 package my.cardholder.ui.cardholder.search
 
 import android.transition.TransitionInflater
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import my.cardholder.databinding.FragmentCardholderSearchBinding
 import my.cardholder.ui.base.BaseFragment
+import my.cardholder.ui.cardholder.cards.CardholderCardsListAdapter
 
 @AndroidEntryPoint
 class CardholderSearchFragment : BaseFragment<FragmentCardholderSearchBinding>(
@@ -13,11 +17,41 @@ class CardholderSearchFragment : BaseFragment<FragmentCardholderSearchBinding>(
 
     override val viewModel: CardholderSearchViewModel by viewModels()
 
+    private val listAdapter by lazy {
+        CardholderCardsListAdapter(
+            onItemClick = { cardId, sharedElements ->
+                val extras = FragmentNavigator.Extras.Builder()
+                    .addSharedElements(sharedElements)
+                    .build()
+                viewModel.onCardClicked(cardId, extras)
+            }
+        )
+    }
+
     override fun initViews() {
         sharedElementEnterTransition = TransitionInflater.from(context)
             .inflateTransition(android.R.transition.move)
+        binding.searchTextInputLayout.apply {
+            editText?.doAfterTextChanged { viewModel.onSearchTextChanged(it?.toString()) }
+        }
+        binding.searchResultsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = listAdapter
+        }
     }
 
     override fun collectData() {
+        viewModel.state.collectWhenStarted { state ->
+            when (state) {
+                is CardholderSearchState.Empty ->  {
+                    binding.searchEmptyMessageText.text = getString(state.messageRes)
+                    listAdapter.submitList(null)
+                }
+                is CardholderSearchState.Success -> {
+                    binding.searchEmptyMessageText.text = null
+                    listAdapter.submitList(state.cards)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchViewModel.kt
+++ app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchViewModel.kt
@@ -1,8 +1,59 @@
 package my.cardholder.ui.cardholder.search
 
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import my.cardholder.R
+import my.cardholder.data.CardRepository
 import my.cardholder.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CardholderSearchViewModel @Inject constructor() : BaseViewModel()
+class CardholderSearchViewModel @Inject constructor(
+    private val cardRepository: CardRepository,
+) : BaseViewModel() {
+
+    private companion object {
+        const val MIN_UPDATE_INTERVAL_MILLIS = 250L
+    }
+
+    private var newSearchRequestText: String? = null
+
+    private val _state = MutableStateFlow<CardholderSearchState>(
+        CardholderSearchState.Empty(R.string.search_blank_message)
+    )
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                delay(MIN_UPDATE_INTERVAL_MILLIS)
+                newSearchRequestText?.let { name ->
+                    newSearchRequestText = null
+                    _state.value = if (name.isBlank()) {
+                        CardholderSearchState.Empty(R.string.search_blank_message)
+                    } else {
+                        val cards = cardRepository.searchForCardsWithNamesLike(name)
+                        if (cards.isNotEmpty()) {
+                            CardholderSearchState.Success(cards)
+                        } else {
+                            CardholderSearchState.Empty(R.string.search_nothing_found_message)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun onSearchTextChanged(cardName: String?) {
+        newSearchRequestText = cardName
+    }
+
+    fun onCardClicked(cardId: Long, extras: Navigator.Extras) {
+        navigate(CardholderSearchFragmentDirections.fromSearchToViewer(cardId), extras)
+    }
+}
