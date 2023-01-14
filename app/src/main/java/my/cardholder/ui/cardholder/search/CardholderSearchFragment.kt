51a2c411d5404886217e36808fe7572fd328da40
kitfist0 <kitfist0@yandex.ru>
2023-01-14 16:17:49 +0300

    Show keyboard on search fragment start

--- app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchFragment.kt
+++ app/src/main/java/my/cardholder/ui/cardholder/search/CardholderSearchFragment.kt
@@ -1,6 +1,10 @@
 package my.cardholder.ui.cardholder.search
 
+import android.content.Context
 import android.transition.TransitionInflater
+import android.transition.TransitionSet
+import android.view.inputmethod.InputMethodManager
+import androidx.core.transition.doOnEnd
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.FragmentNavigator
@@ -38,12 +42,17 @@ class CardholderSearchFragment : BaseFragment<FragmentCardholderSearchBinding>(
             layoutManager = LinearLayoutManager(context)
             adapter = listAdapter
         }
+        binding.searchTextInputLayout.requestFocus()
+        (sharedElementEnterTransition as TransitionSet).doOnEnd {
+            (context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
+                ?.showSoftInput(binding.searchTextInputLayout.editText, 1)
+        }
     }
 
     override fun collectData() {
         viewModel.state.collectWhenStarted { state ->
             when (state) {
-                is CardholderSearchState.Empty ->  {
+                is CardholderSearchState.Empty -> {
                     binding.searchEmptyMessageText.text = getString(state.messageRes)
                     listAdapter.submitList(null)
                 }
