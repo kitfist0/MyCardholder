b8efd12099860fffb6a358242730474ae67aea36
kitfist0 <kitfist0@yandex.ru>
2023-01-21 15:03:59 +0300

    Add delete card dialog and viewmodel

---
+++ app/src/main/java/my/cardholder/ui/cardholder/delete/CardholderDeleteCardViewModel.kt
@@ -0,0 +1,29 @@
+package my.cardholder.ui.cardholder.delete
+
+import androidx.lifecycle.viewModelScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import my.cardholder.data.CardRepository
+import my.cardholder.ui.base.BaseViewModel
+
+class CardholderDeleteCardViewModel @AssistedInject constructor(
+    @Assisted("card_id") private val cardId: Long,
+    private val cardRepository: CardRepository,
+) : BaseViewModel() {
+    fun onDeleteButtonClicked() {
+        viewModelScope.launch {
+            cardRepository.deleteCard(cardId)
+            navigateUp()
+        }
+    }
+}
+
+@AssistedFactory
+interface CardholderDeleteCardViewModelFactory {
+    fun create(
+        @Assisted("card_id") cardId: Long,
+    ): CardholderDeleteCardViewModel
+}
