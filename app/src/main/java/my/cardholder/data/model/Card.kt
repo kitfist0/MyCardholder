30c207582d179d6f120a23da77e53b408f89f6fc
kitfist0 <kitfist0@yandex.ru>
2023-01-29 20:15:57 +0300

    Add new card colors

--- app/src/main/java/my/cardholder/data/model/Card.kt
+++ app/src/main/java/my/cardholder/data/model/Card.kt
@@ -21,13 +21,17 @@ data class Card(
     companion object {
         val COLORS = arrayOf(
             "#EF5350",
+            "#EC407A",
             "#7E57C2",
             "#5C6BC0",
+            "#42A5F5",
             "#26A69A",
             "#66BB6A",
             "#FFCA28",
             "#FF7043",
             "#8D6E63",
+            "#424242",
+            "#E0E0E0",
         )
 
         fun Card.getColorInt() = try {
