7402604eba8b4746f8902101b4392363c759fe58
kitfist0 <kitfist0@yandex.ru>
2023-02-09 21:13:37 +0300

    Add logic for about fragment and viewmodel

--- app/src/main/java/my/cardholder/ui/settings/about/SettingsAboutDialog.kt
+++ app/src/main/java/my/cardholder/ui/settings/about/SettingsAboutDialog.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import my.cardholder.databinding.DialogSettingsAboutBinding
 import my.cardholder.ui.base.BaseDialogFragment
+import my.cardholder.util.ext.collectWhenStarted
 
 @AndroidEntryPoint
 class SettingsAboutDialog : BaseDialogFragment<DialogSettingsAboutBinding>(
@@ -13,8 +14,22 @@ class SettingsAboutDialog : BaseDialogFragment<DialogSettingsAboutBinding>(
     override val viewModel: SettingsAboutViewModel by viewModels()
 
     override fun initViews() {
+        with(binding) {
+            aboutPolicyButton.setOnClickListener {
+                viewModel.onPrivacyPolicyButtonClicked()
+            }
+            aboutSourceCodeButton.setOnClickListener {
+                viewModel.onSourceCodeButtonClicked()
+            }
+            aboutCopyrightButton.setOnClickListener {
+                viewModel.onCopyrightButtonClicked()
+            }
+        }
     }
 
     override fun collectData() {
+        collectWhenStarted(viewModel.dialogTitle) { title ->
+            binding.aboutDevTitleText.text = title
+        }
     }
 }
