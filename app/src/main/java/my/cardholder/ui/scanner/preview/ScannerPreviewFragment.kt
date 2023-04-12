02ac6073f5dcd450691fb2136f497d3a95fdaf68
kitfist0 <kitfist0@yandex.ru>
2023-04-12 21:34:50 +0300

    Animate scanner preview explanation

--- app/src/main/java/my/cardholder/ui/scanner/preview/ScannerPreviewFragment.kt
+++ app/src/main/java/my/cardholder/ui/scanner/preview/ScannerPreviewFragment.kt
@@ -1,16 +1,24 @@
 package my.cardholder.ui.scanner.preview
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import my.cardholder.databinding.FragmentScannerPreviewBinding
 import my.cardholder.ui.base.BaseFragment
+import my.cardholder.util.ext.collectWhenStarted
 
 @AndroidEntryPoint
 class ScannerPreviewFragment : BaseFragment<FragmentScannerPreviewBinding>(
     FragmentScannerPreviewBinding::inflate
 ) {
+
+    private companion object {
+        const val ALPHA_ANIM_DURATION = 300L
+    }
 
     override val viewModel: ScannerPreviewViewModel by viewModels()
 
@@ -24,5 +32,23 @@ class ScannerPreviewFragment : BaseFragment<FragmentScannerPreviewBinding>(
     }
 
     override fun collectData() {
+        collectWhenStarted(viewModel.state) { state ->
+            binding.scannerExplanationText.apply {
+                if (!state.withExplanation) {
+                    animate()
+                        .alpha(0f)
+                        .setDuration(ALPHA_ANIM_DURATION)
+                        .setListener(
+                            object : AnimatorListenerAdapter() {
+                                override fun onAnimationEnd(animation: Animator) {
+                                    isVisible = false
+                                }
+                            }
+                        )
+                } else {
+                    isVisible = true
+                }
+            }
+        }
     }
 }
