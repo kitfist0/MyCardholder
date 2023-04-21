7b0b21bd3c1c203700d7aa6ea79a6bb16f55a4a4
kitfist0 <kitfist0@yandex.ru>
2023-04-21 22:11:48 +0300

    Rename coffee dialog layout

--- app/src/main/java/my/cardholder/ui/coffee/CoffeeDialog.kt
+++ app/src/main/java/my/cardholder/ui/coffee/CoffeeDialog.kt
@@ -3,13 +3,13 @@ package my.cardholder.ui.coffee
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import my.cardholder.databinding.DialogSettingsCoffeeBinding
+import my.cardholder.databinding.DialogCoffeeBinding
 import my.cardholder.ui.base.BaseDialogFragment
 import my.cardholder.util.ext.collectWhenStarted
 
 @AndroidEntryPoint
-class CoffeeDialog : BaseDialogFragment<DialogSettingsCoffeeBinding>(
-    DialogSettingsCoffeeBinding::inflate
+class CoffeeDialog : BaseDialogFragment<DialogCoffeeBinding>(
+    DialogCoffeeBinding::inflate
 ) {
 
     private val listAdapter by lazy(LazyThreadSafetyMode.NONE) {
