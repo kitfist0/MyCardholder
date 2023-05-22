a356c033802c029208388bcdd061084996eba873
kitfist0 <kitfist0@yandex.ru>
2023-05-22 22:49:40 +0300

    Remove room type converters

--- app/src/main/java/my/cardholder/data/AppDatabase.kt
+++ app/src/main/java/my/cardholder/data/AppDatabase.kt
@@ -2,7 +2,6 @@ package my.cardholder.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import my.cardholder.data.model.Card
 import my.cardholder.data.model.Coffee
 import my.cardholder.data.model.Label
@@ -12,7 +11,6 @@ import my.cardholder.data.model.Label
     version = 1,
     exportSchema = false,
 )
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cardDao(): CardDao
     abstract fun coffeeDao(): CoffeeDao
