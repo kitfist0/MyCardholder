ac1033d42c39cf449205449756e2b74c4202a041
kitfist0 <kitfist0@yandex.ru>
2023-06-16 22:29:26 +0300

    Refactor purchase flow launcher

--- app/src/main/java/my/cardholder/util/PurchaseFlowLauncher.kt
+++ app/src/main/java/my/cardholder/util/PurchaseFlowLauncher.kt
@@ -1,72 +1,20 @@
 package my.cardholder.util
 
 import android.app.Activity
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingFlowParams
-import com.android.billingclient.api.ProductDetails
-import com.android.billingclient.api.ProductDetailsResult
-import com.android.billingclient.api.QueryProductDetailsParams
-import com.android.billingclient.api.queryProductDetails
-import dagger.hilt.android.scopes.ActivityScoped
-import my.cardholder.util.ext.getErrorMessage
-import my.cardholder.util.ext.isOk
+import my.cardholder.util.ext.launchNonConsumableProductPurchase
 import javax.inject.Inject
 
-@ActivityScoped
 class PurchaseFlowLauncher @Inject constructor(
-    private val activity: Activity,
     private val playBillingWrapper: PlayBillingWrapper,
 ) {
-
-    suspend fun startPurchase(productId: String): Result<Unit> {
+    suspend fun startPurchase(activity: Activity, productId: String): Result<Unit> {
         return playBillingWrapper.getClient().fold(
             onSuccess = { billingClient ->
-                val productDetailsResult = billingClient.queryNonConsumableProductDetails(productId)
-                val productDetailsBillingResult = productDetailsResult.billingResult
-                val productDetailsList = productDetailsResult.productDetailsList
-                if (productDetailsBillingResult.isOk() && !productDetailsList.isNullOrEmpty()) {
-                    billingClient.launchBillingFlowForResult(productDetailsList.first())
-                } else {
-                    Result.failure(Throwable(productDetailsBillingResult.getErrorMessage()))
-                }
+                billingClient.launchNonConsumableProductPurchase(activity, productId)
             },
-            onFailure = {
-                Result.failure(it)
+            onFailure = { throwable ->
+                Result.failure(throwable)
             }
         )
-    }
-
-    private fun BillingClient.launchBillingFlowForResult(
-        productDetails: ProductDetails,
-    ): Result<Unit> {
-        val productDetailsParamsList = listOf(
-            BillingFlowParams.ProductDetailsParams.newBuilder()
-                .setProductDetails(productDetails)
-                .build()
-        )
-        val billingFlowParams = BillingFlowParams.newBuilder()
-            .setProductDetailsParamsList(productDetailsParamsList)
-            .build()
-        val billingResult = launchBillingFlow(activity, billingFlowParams)
-        return if (billingResult.isOk()) {
-            Result.success(Unit)
-        } else {
-            Result.failure(Throwable(billingResult.getErrorMessage()))
-        }
-    }
-
-    private suspend fun BillingClient.queryNonConsumableProductDetails(
-        vararg productIds: String,
-    ): ProductDetailsResult {
-        val productList = productIds.map { productId ->
-            QueryProductDetailsParams.Product.newBuilder()
-                .setProductId(productId)
-                .setProductType(BillingClient.ProductType.INAPP)
-                .build()
-        }
-        val queryProductDetailsParams = QueryProductDetailsParams.newBuilder()
-            .setProductList(productList)
-            .build()
-        return queryProductDetails(queryProductDetailsParams)
     }
 }
