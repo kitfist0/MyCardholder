02224f6097f727bac4980cbf3facfac2f313bf49
kitfist0 <kitfist0@yandex.ru>
2023-06-22 20:51:19 +0300

    Add category dao

---
+++ app/src/main/java/my/cardholder/data/source/CategoryDao.kt
@@ -0,0 +1,25 @@
+package my.cardholder.data.source
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+import my.cardholder.data.model.Category
+
+@Dao
+interface CategoryDao {
+    @Query("SELECT * FROM categories")
+    fun getCategories(): Flow<List<Category>>
+
+    @Query("SELECT * FROM categories WHERE id = :id")
+    suspend fun getCategoryById(id: Long): Category?
+
+    @Query("SELECT * FROM categories WHERE name = :name")
+    suspend fun getCategoryByName(name: String): Category?
+
+    @Query("DELETE FROM categories WHERE id = :id")
+    suspend fun deleteCategoryById(id: Long)
+
+    @Upsert
+    suspend fun upsert(category: Category)
+}
