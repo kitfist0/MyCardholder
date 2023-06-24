8256e6971c8dcb2374565970bffe3062e5c182b0
kitfist0 <kitfist0@yandex.ru>
2023-06-24 15:47:44 +0300

    Add card and categories to card list state

---
+++ app/src/main/java/my/cardholder/ui/card/list/CardListAdapter.kt
@@ -0,0 +1,92 @@
+package my.cardholder.ui.card.list
+
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.ColorUtils
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import my.cardholder.data.model.Card.Companion.getColorInt
+import my.cardholder.data.model.CardAndCategory
+import my.cardholder.databinding.ItemCardBinding
+import my.cardholder.util.ext.loadBarcodeImage
+import my.cardholder.util.ext.setupUniqueTransitionName
+
+class CardListAdapter(
+    private val onItemClicked: (cardId: Long, sharedElements: Map<View, String>) -> Unit,
+) : ListAdapter<CardAndCategory, CardListAdapter.CardViewHolder>(CardDiffCallback) {
+
+    private companion object {
+        object CardDiffCallback : DiffUtil.ItemCallback<CardAndCategory>() {
+            override fun areItemsTheSame(oldItem: CardAndCategory, newItem: CardAndCategory) =
+                oldItem.card.id == newItem.card.id
+
+            override fun areContentsTheSame(oldItem: CardAndCategory, newItem: CardAndCategory) =
+                oldItem == newItem
+        }
+    }
+
+    inner class CardViewHolder(
+        private val binding: ItemCardBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+                val cardAndCategory = getItem(adapterPosition)
+                val sharedElements = with(binding) {
+                    mapOf(
+                        itemCardBarcodeImage to itemCardBarcodeImage.transitionName,
+                        itemCardNameText to itemCardNameText.transitionName,
+                        itemCardContentText to itemCardContentText.transitionName,
+                        itemCardCategoryText to itemCardCategoryText.transitionName,
+                    )
+                }
+                onItemClicked.invoke(cardAndCategory.card.id, sharedElements)
+            }
+        }
+
+        fun bind(cardAndCategory: CardAndCategory) {
+            with(binding) {
+                val card = cardAndCategory.card
+                val uniqueNameSuffix = card.id
+                itemCardLayout.background = getCardGradientDrawable(card.getColorInt())
+                itemCardBarcodeImage.apply {
+                    setupUniqueTransitionName(uniqueNameSuffix)
+                    loadBarcodeImage(
+                        barcodeFile = card.barcodeFile,
+                        originalSize = false,
+                    )
+                }
+                itemCardNameText.apply {
+                    setupUniqueTransitionName(uniqueNameSuffix)
+                    text = card.name
+                }
+                itemCardContentText.apply {
+                    setupUniqueTransitionName(uniqueNameSuffix)
+                    text = card.content
+                }
+                itemCardCategoryText.apply {
+                    setupUniqueTransitionName(uniqueNameSuffix)
+                    text = cardAndCategory.category.name
+                }
+            }
+        }
+
+        private fun getCardGradientDrawable(colorInt: Int): GradientDrawable {
+            val bottomLeftColor = ColorUtils.blendARGB(colorInt, Color.TRANSPARENT, 0.2f)
+            return GradientDrawable(GradientDrawable.Orientation.BL_TR, intArrayOf(bottomLeftColor, colorInt))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val binding = ItemCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CardViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
--- app/src/main/java/my/cardholder/ui/card/list/CardListState.kt
+++ app/src/main/java/my/cardholder/ui/card/list/CardListState.kt
@@ -3,8 +3,12 @@ package my.cardholder.ui.card.list
 import androidx.annotation.StringRes
 import my.cardholder.R
 import my.cardholder.data.model.Card
+import my.cardholder.data.model.CardAndCategory
 
 sealed class CardListState {
     data class Empty(@StringRes val messageRes: Int = R.string.card_list_no_cards_message) : CardListState()
-    data class Success(val cards: List<Card>, val spanCount: Int) : CardListState()
+    data class Success(
+        val cardsAndCategories: List<CardAndCategory>,
+        val spanCount: Int,
+    ) : CardListState()
 }
