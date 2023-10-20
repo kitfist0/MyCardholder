62f3ef83dc9e00c4ba69b2b6fc0b24188d91c8db
kitfist0 <kitfist0@yandex.ru>
2023-10-20 19:31:40 +0300

    Add splash screen api support

--- app/src/main/java/my/cardholder/ui/MainActivity.kt
+++ app/src/main/java/my/cardholder/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
