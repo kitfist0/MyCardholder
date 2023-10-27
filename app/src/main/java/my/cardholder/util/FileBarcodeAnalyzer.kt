a7b6d24f2a92d4eb380e00fb1a83accb5860d7be
kitfist0 <kitfist0@yandex.ru>
2023-10-27 21:36:13 +0300

    Add file barcode analyzer

---
+++ app/src/main/java/my/cardholder/util/FileBarcodeAnalyzer.kt
@@ -0,0 +1,30 @@
+package my.cardholder.util
+
+import android.content.Context
+import android.net.Uri
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import java.io.File
+
+class FileBarcodeAnalyzer(
+    private val barcodeScanner: BarcodeScanner,
+    private val context: Context,
+) {
+
+    private val _barcode = MutableStateFlow<Barcode?>(null)
+    val barcode = _barcode.asStateFlow().filterNotNull()
+
+    fun analyze(file: File) {
+        val inputImage = InputImage.fromFilePath(context, Uri.fromFile(file))
+        barcodeScanner.process(inputImage)
+            .addOnSuccessListener { barcodes ->
+                if (barcodes.isNotEmpty()) {
+                    _barcode.value = barcodes.first()
+                }
+            }
+    }
+}
