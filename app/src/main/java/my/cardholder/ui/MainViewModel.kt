31ea7938637df086c8c51e24250ab569f3e21861
kitfist0 <kitfist0@yandex.ru>
2024-02-23 18:13:36 +0300

    Move purchase status update to main viewmodel

--- app/src/main/java/my/cardholder/ui/MainViewModel.kt
+++ app/src/main/java/my/cardholder/ui/MainViewModel.kt
@@ -5,16 +5,31 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import my.cardholder.billing.PurchasedProductsProvider
+import my.cardholder.data.CoffeeRepository
 import my.cardholder.data.SettingsRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    purchasedProductsProvider: PurchasedProductsProvider,
     settingsRepository: SettingsRepository,
+    private val coffeeRepository: CoffeeRepository,
 ) : ViewModel() {
+
     val nightModeEnabled = settingsRepository.nightModeEnabled.stateIn(
         scope = viewModelScope,
         started = SharingStarted.Eagerly,
         initialValue = false
     )
+
+    init {
+        viewModelScope.launch {
+            coffeeRepository.initialize()
+            purchasedProductsProvider.purchasedProducts.collect { purchasedIds ->
+                coffeeRepository.updatePurchaseStatusOfCoffees(purchasedIds)
+            }
+        }
+    }
 }
--- app/src/main/java/my/cardholder/ui/coffee/CoffeeViewModel.kt
+++ app/src/main/java/my/cardholder/ui/coffee/CoffeeViewModel.kt
@@ -6,17 +6,14 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import my.cardholder.data.CoffeeRepository
 import my.cardholder.ui.base.BaseViewModel
-import my.cardholder.billing.PurchasedProductsProvider
 import javax.inject.Inject
 
 @HiltViewModel
 class CoffeeViewModel @Inject constructor(
-    purchasedProductsProvider: PurchasedProductsProvider,
-    private val coffeeRepository: CoffeeRepository,
+    coffeeRepository: CoffeeRepository,
 ) : BaseViewModel() {
 
     private val _state = MutableStateFlow(
@@ -26,15 +23,8 @@ class CoffeeViewModel @Inject constructor(
 
     init {
         coffeeRepository.coffees
-            .onStart { coffeeRepository.initialize() }
             .onEach { coffees ->
                 _state.update { it.copy(coffees = coffees) }
-            }
-            .launchIn(viewModelScope)
-
-        purchasedProductsProvider.purchasedProducts
-            .onEach { purchasedIds ->
-                coffeeRepository.updatePurchaseStatusOfCoffees(purchasedIds)
             }
             .launchIn(viewModelScope)
     }
