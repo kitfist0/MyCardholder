0a59cea09e69a5db784aed17e352044b903a2bf3
kitfist0 <kitfist0@yandex.ru>
2024-03-02 16:21:47 +0300

    Add translations for card action dialog

--- app/src/main/java/my/cardholder/ui/card/action/CardActionState.kt
+++ app/src/main/java/my/cardholder/ui/card/action/CardActionState.kt
@@ -1,5 +1,6 @@
 package my.cardholder.ui.card.action
 
+import my.cardholder.R
 import my.cardholder.util.Text
 
 data class CardActionState(
@@ -8,11 +9,20 @@ data class CardActionState(
 ) {
     companion object {
         fun CardActionState.getTitleText(): Text {
-            return Text.Simple("Choose action with card \"%s\"".format(cardName))
+            return Text.ResourceAndParams(
+                R.string.card_action_dialog_default_title,
+                listOf(cardName)
+            )
         }
 
         fun CardActionState.getPinUnpinButtonText(): Text {
-            return Text.Simple(if (isPinned) "Unpin card" else "Pin card")
+            return Text.Resource(
+                if (isPinned) {
+                    R.string.card_action_dialog_unpin_card_button_text
+                } else {
+                    R.string.card_action_dialog_pin_card_button_text
+                }
+            )
         }
     }
 }
