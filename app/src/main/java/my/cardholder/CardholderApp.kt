3037399bcd79f7bcd9d2a003e6d124b50c0db655
kitfist0 <kitfist0@yandex.ru>
2024-05-02 22:04:25 +0300

    Add work manager configuration

--- app/src/main/java/my/cardholder/CardholderApp.kt
+++ app/src/main/java/my/cardholder/CardholderApp.kt
@@ -1,7 +1,17 @@
 package my.cardholder
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class CardholderApp : Application()
+class CardholderApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory : HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder().setWorkerFactory(workerFactory).build()
+}
