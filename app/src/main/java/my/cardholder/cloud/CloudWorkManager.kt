004e76492bb27eeb80e906db082385769bd9f159
kitfist0 <kitfist0@yandex.ru>
2024-05-15 22:22:36 +0300

    Add cloud work manager

---
+++ app/src/main/java/my/cardholder/cloud/CloudWorkManager.kt
@@ -0,0 +1,34 @@
+package my.cardholder.cloud
+
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+import javax.inject.Inject
+
+class CloudWorkManager @Inject constructor(
+    private val workManager: WorkManager,
+) {
+
+    private companion object {
+        const val CLEANUP_WORK_NAME_PREFIX = "cleanup_"
+        const val UPLOAD_WORK_NAME_PREFIX = "upload_"
+    }
+
+    val uploadWorkInfoList = workManager.getWorkInfosByTagFlow(CloudUploadWorker::class.java.name)
+
+    fun enqueueCleanupWork(uniqueId: Long) {
+        workManager.cancelUniqueWork(UPLOAD_WORK_NAME_PREFIX + uniqueId)
+        val uniqueWorkName = CLEANUP_WORK_NAME_PREFIX + uniqueId
+        val workRequest = CloudCleanupWorker.getWorkRequest(listOf(uniqueId.toString()))
+        workManager.enqueueUniqueWork(uniqueWorkName, ExistingWorkPolicy.REPLACE, workRequest)
+    }
+
+    fun enqueueUploadWork(uniqueId: Long, content: String) {
+        val uniqueWorkName = UPLOAD_WORK_NAME_PREFIX + uniqueId
+        val workRequest = CloudUploadWorker.getWorkRequest(listOf(uniqueId.toString() to content))
+        workManager.enqueueUniqueWork(uniqueWorkName, ExistingWorkPolicy.REPLACE, workRequest)
+    }
+
+    fun cancelAllWork() {
+        workManager.cancelAllWork()
+    }
+}
