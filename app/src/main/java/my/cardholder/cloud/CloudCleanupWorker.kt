794b6fc45b0cd01fb9e5fff8af3ba6f566fa43bc
kitfist0 <kitfist0@yandex.ru>
2024-05-18 16:51:25 +0300

    Make cloud work periodic

--- app/src/main/java/my/cardholder/cloud/CloudCleanupWorker.kt
+++ app/src/main/java/my/cardholder/cloud/CloudCleanupWorker.kt
@@ -6,10 +6,11 @@ import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import java.util.concurrent.TimeUnit
 
 @HiltWorker
 class CloudCleanupWorker @AssistedInject constructor(
@@ -19,31 +20,39 @@ class CloudCleanupWorker @AssistedInject constructor(
 ) : CoroutineWorker(context, params) {
 
     companion object {
-        private const val FILE_NAME_KEY = "file_name"
+        private const val REPEAT_INTERVAL_MINUTES = 15
+        private const val FLEX_INTERVAL_MINUTES = 10
+        private const val FILE_NAMES_KEY = "file_names"
 
-        fun getWorkRequest(fileName: String): OneTimeWorkRequest {
+        fun getPeriodicWorkRequest(vararg fileName: String): PeriodicWorkRequest {
             val inputData = Data.Builder()
-                .putString(FILE_NAME_KEY, fileName)
+                .putStringArray(FILE_NAMES_KEY, fileName)
                 .build()
             val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build()
-            return OneTimeWorkRequest.Builder(CloudCleanupWorker::class.java)
-                .setConstraints(constraints)
-                .setInputData(inputData)
-                .build()
+            val workRequestBuilder = PeriodicWorkRequest.Builder(
+                CloudCleanupWorker::class.java,
+                REPEAT_INTERVAL_MINUTES.toLong(), TimeUnit.MINUTES,
+                FLEX_INTERVAL_MINUTES.toLong(), TimeUnit.MINUTES
+            ).apply {
+                setConstraints(constraints)
+                setInputData(inputData)
+            }
+            return workRequestBuilder.build()
         }
     }
 
     override suspend fun doWork(): Result {
-        val fileName = params.inputData.getString(FILE_NAME_KEY)
-        return fileName?.let { name ->
-            val uploadResult = cloudAssistant.delete(name)
-            if (uploadResult.isSuccess) {
-                Result.success()
-            } else {
-                Result.retry()
-            }
-        } ?: Result.failure()
+        val fileNames = params.inputData.getStringArray(FILE_NAMES_KEY)
+        if (fileNames.isNullOrEmpty()) {
+            return Result.success()
+        }
+        val uploadResult = cloudAssistant.delete(*fileNames)
+        return if (uploadResult.isSuccess) {
+            Result.success()
+        } else {
+            Result.failure()
+        }
     }
 }
--- app/src/main/java/my/cardholder/cloud/CloudWorkManager.kt
+++ app/src/main/java/my/cardholder/cloud/CloudWorkManager.kt
@@ -1,6 +1,6 @@
 package my.cardholder.cloud
 
-import androidx.work.ExistingWorkPolicy
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
 import javax.inject.Inject
 
@@ -9,28 +9,26 @@ class CloudWorkManager @Inject constructor(
 ) {
 
     private companion object {
-        const val CLEANUP_WORK_NAME_PREFIX = "cleanup_"
-        const val UPLOAD_WORK_NAME_PREFIX = "upload_"
+        const val CLEANUP_WORK_NAME = "cleanup_work"
+        const val UPLOAD_WORK_NAME = "upload_work"
     }
 
-    val uploadWorkInfoList = workManager.getWorkInfosByTagFlow(CloudUploadWorker::class.java.name)
-
-    fun enqueueCleanupWork(uniqueId: Long) {
-        workManager.cancelUniqueWork(UPLOAD_WORK_NAME_PREFIX + uniqueId)
-        val uniqueWorkName = CLEANUP_WORK_NAME_PREFIX + uniqueId
-        val workRequest = CloudCleanupWorker.getWorkRequest(
-            fileName = uniqueId.toString(),
+    fun enqueueCleanupWork(vararg fileName: String) {
+        val workRequest = CloudCleanupWorker.getPeriodicWorkRequest(*fileName)
+        workManager.enqueueUniquePeriodicWork(
+            CLEANUP_WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
         )
-        workManager.enqueueUniqueWork(uniqueWorkName, ExistingWorkPolicy.REPLACE, workRequest)
     }
 
-    fun enqueueUploadWork(uniqueId: Long, content: String) {
-        val uniqueWorkName = UPLOAD_WORK_NAME_PREFIX + uniqueId
-        val workRequest = CloudUploadWorker.getWorkRequest(
-            fileName = uniqueId.toString(),
-            fileContent = content,
+    fun enqueueUploadWork(vararg fileNameAndContent: FileNameAndContent) {
+        val workRequest = CloudUploadWorker.getPeriodicWorkRequest(*fileNameAndContent)
+        workManager.enqueueUniquePeriodicWork(
+            UPLOAD_WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
         )
-        workManager.enqueueUniqueWork(uniqueWorkName, ExistingWorkPolicy.REPLACE, workRequest)
     }
 
     fun cancelAllWork() {
