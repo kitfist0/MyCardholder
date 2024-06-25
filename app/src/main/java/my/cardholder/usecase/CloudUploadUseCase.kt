0a13d0fb687d43f5d246eefbef267f7849b27cef
kitfist0 <kitfist0@yandex.ru>
2024-06-25 23:10:09 +0300

    Refactor cloud upload use case

--- app/src/main/java/my/cardholder/usecase/CloudUploadUseCase.kt
+++ app/src/main/java/my/cardholder/usecase/CloudUploadUseCase.kt
@@ -5,12 +5,13 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.map
 import my.cardholder.cloud.backup.BackupChecksum
 import my.cardholder.cloud.backup.CloudBackupAssistant
 import my.cardholder.data.BackupRepository
 import my.cardholder.data.SettingsRepository
 import my.cardholder.data.model.BackupResult
+import my.cardholder.util.Result
 import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 
@@ -20,26 +21,39 @@ class CloudUploadUseCase @Inject constructor(
     private val settingsRepository: SettingsRepository,
 ) {
 
-    fun execute(checksum: BackupChecksum): Flow<BackupResult> = uploadBackup(checksum)
+    private companion object {
+        const val SUCCESS_MESSAGE = "Upload completed!"
+    }
+
+    fun execute(checksum: BackupChecksum): Flow<Result<String>> = uploadBackup(checksum)
 
     private fun uploadBackup(
         checksum: BackupChecksum,
         outputStream: ByteArrayOutputStream = ByteArrayOutputStream(),
-    ): Flow<BackupResult> = flow {
+    ): Flow<Result<String>> = flow {
         if (!settingsRepository.cloudSyncEnabled.first()) {
             throw Throwable("Cloud sync disabled!")
         }
+        emit(Result.Loading("Getting the backup checksum"))
         if (checksum == cloudBackupAssistant.getBackupChecksum().getOrNull()) {
-            throw Throwable("Cloud sync has already been completed!")
-        }
-        emitAll(backupRepository.exportToBackupFile(outputStream))
-    }.onEach { backupResult ->
-        if (backupResult is BackupResult.Success) {
-            val content = String(outputStream.toByteArray())
-            cloudBackupAssistant.uploadBackup(content, checksum)
-                .onSuccess { settingsRepository.setLatestSyncedBackupChecksum(checksum) }
+            emit(Result.Success(SUCCESS_MESSAGE))
+        } else {
+            val resultFlow = backupRepository.exportToBackupFile(outputStream)
+                .map { backupResult ->
+                    when (backupResult) {
+                        is BackupResult.Error -> throw Throwable(backupResult.message)
+                        is BackupResult.Progress -> Result.Loading("Uploading backup data")
+                        is BackupResult.Success -> {
+                            cloudBackupAssistant.uploadBackup(String(outputStream.toByteArray()), checksum)
+                                .onFailure { throwable -> throw throwable }
+                            settingsRepository.setLatestSyncedBackupChecksum(checksum)
+                            Result.Success(SUCCESS_MESSAGE)
+                        }
+                    }
+                }
+            emitAll(resultFlow)
         }
     }.catch {
-        emit(BackupResult.Error(it.message.orEmpty()))
+        emit(Result.Error(it))
     }
 }
