3cc84444c6d2707035107153873f72cb99b34760
kitfist0 <kitfist0@yandex.ru>
2024-09-12 20:55:06 +0300

    Add logic for card crop fragment and viewmodel

--- app/src/main/java/my/cardholder/ui/card/crop/CardCropFragment.kt
+++ app/src/main/java/my/cardholder/ui/card/crop/CardCropFragment.kt
@@ -1,9 +1,13 @@
 package my.cardholder.ui.card.crop
 
+import android.net.Uri
 import androidx.fragment.app.viewModels
+import com.canhub.cropper.CropImageOptions
+import com.google.mlkit.vision.common.InputImage
 import dagger.hilt.android.AndroidEntryPoint
 import my.cardholder.databinding.FragmentCardCropBinding
 import my.cardholder.ui.base.BaseFragment
+import my.cardholder.util.ext.collectWhenStarted
 
 @AndroidEntryPoint
 class CardCropFragment : BaseFragment<FragmentCardCropBinding>(
@@ -13,8 +17,28 @@ class CardCropFragment : BaseFragment<FragmentCardCropBinding>(
     override val viewModel: CardCropViewModel by viewModels()
 
     override fun initViews() {
+        with(binding) {
+            cardCropImageView.setImageCropOptions(CropImageOptions())
+            cardCropImageView.setOnCropImageCompleteListener { _, result ->
+                val inputImage = result.bitmap
+                    ?.let { InputImage.fromBitmap(it, 0) }
+                viewModel.onCropCompleted(inputImage)
+            }
+            cardCropOkFab.setOnClickListener {
+                viewModel.onOkFabClicked()
+            }
+        }
     }
 
     override fun collectData() {
+        collectWhenStarted(viewModel.state) { state ->
+            binding.cardCropImageView.apply {
+                imageUri ?: setImageUriAsync(Uri.parse(state.selectedImageUri))
+            }
+            if (state.cropButtonClickEvent) {
+                binding.cardCropImageView.croppedImageAsync()
+                viewModel.consumeCropButtonClickEvent()
+            }
+        }
     }
 }
--- app/src/main/java/my/cardholder/ui/card/crop/CardCropViewModel.kt
+++ app/src/main/java/my/cardholder/ui/card/crop/CardCropViewModel.kt
@@ -1,8 +1,66 @@
 package my.cardholder.ui.card.crop
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.google.mlkit.vision.common.InputImage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import my.cardholder.R
+import my.cardholder.data.CardRepository
+import my.cardholder.data.ScanResultRepository
+import my.cardholder.data.model.ScanResult
 import my.cardholder.ui.base.BaseViewModel
+import my.cardholder.util.Text
 import javax.inject.Inject
 
 @HiltViewModel
-class CardCropViewModel @Inject constructor() : BaseViewModel()
+class CardCropViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val cardRepository: CardRepository,
+    private val scanResultRepository: ScanResultRepository,
+) : BaseViewModel() {
+
+    private val _state = MutableStateFlow(
+        CardCropState(
+            selectedImageUri = CardCropFragmentArgs.fromSavedStateHandle(savedStateHandle).imageUri,
+            cropButtonClickEvent = false,
+        )
+    )
+    val state = _state.asStateFlow()
+
+    init {
+        scanResultRepository.fileScanResult
+            .onEach { scanResult ->
+                when (scanResult) {
+                    is ScanResult.Success -> {
+                        val cardId = cardRepository.insertNewCard(
+                            content = scanResult.content,
+                            format = scanResult.format
+                        )
+                        navigate(CardCropFragmentDirections.fromCardCropToCardDisplay(cardId))
+                    }
+                    is ScanResult.Failure ->
+                        showSnack(Text.Simple(scanResult.throwable.toString()))
+                    ScanResult.Nothing ->
+                        showSnack(Text.Resource(R.string.snack_message_barcode_not_found))
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    fun onCropCompleted(inputImage: InputImage?) {
+        inputImage?.let { scanResultRepository.scan(it) }
+    }
+
+    fun onOkFabClicked() {
+        _state.update { it.copy(cropButtonClickEvent = true) }
+    }
+
+    fun consumeCropButtonClickEvent() {
+        _state.update { it.copy(cropButtonClickEvent = false) }
+    }
+}
