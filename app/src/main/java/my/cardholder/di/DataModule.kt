6d6b3591dba3bfd00b2dca045f29353619c286ee
kitfist0 <kitfist0@yandex.ru>
2025-07-15 21:44:00 +0300

    Refactor database migrations

--- app/src/main/java/my/cardholder/di/DataModule.kt
+++ app/src/main/java/my/cardholder/di/DataModule.kt
@@ -53,7 +53,7 @@ object DataModule {
                     }
                 }
             )
-            .addMigrations(AppDatabase.MIGRATION_1_2, AppDatabase.MIGRATION_2_3, AppDatabase.MIGRATION_3_4)
+            .addMigrations(*AppDatabase.MIGRATIONS)
             .build()
     }
 
