490aeab92559c97ec5604895e1513783efeb9010
kitfist0 <kitfist0@yandex.ru>
2025-07-30 20:04:29 +0300

    Refactor rustore billing activity

--- app/src/rustore/java/my/cardholder/billing/BillingActivity.kt
+++ app/src/rustore/java/my/cardholder/billing/BillingActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import ru.rustore.sdk.billingclient.utils.resolveForBilling
-import ru.rustore.sdk.core.feature.model.FeatureAvailabilityResult
 import javax.inject.Inject
 
 abstract class BillingActivity : AppCompatActivity(), BillingActivityInterface {
@@ -27,19 +25,6 @@ abstract class BillingActivity : AppCompatActivity(), BillingActivityInterface {
     }
 
     override fun purchaseProduct(productId: String) {
-        ruStoreBillingAssistant.billingClient.purchases.checkPurchasesAvailability()
-            .addOnSuccessListener { result ->
-                when (result) {
-                    is FeatureAvailabilityResult.Available -> launchBillingFlow(productId)
-                    is FeatureAvailabilityResult.Unavailable -> result.cause.resolveForBilling(this)
-                }
-            }
-            .addOnFailureListener { throwable ->
-                showToast(throwable.message)
-            }
-    }
-
-    private fun launchBillingFlow(productId: String) {
         ruStoreBillingAssistant.getBillingFlowParams(productId) { result ->
             result.onSuccess { billingParams ->
                 ruStoreBillingAssistant.billingClient.purchases.purchaseProduct(
