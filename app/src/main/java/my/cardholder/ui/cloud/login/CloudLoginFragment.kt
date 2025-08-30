19552b9207b28d36067bd7f95f1cc2b5b34cb0da
kitfist0 <kitfist0@yandex.ru>
2025-08-30 14:12:58 +0300

    Add logic for cloud login fragment and viewmodel

--- app/src/main/java/my/cardholder/ui/cloud/login/CloudLoginFragment.kt
+++ app/src/main/java/my/cardholder/ui/cloud/login/CloudLoginFragment.kt
@@ -1,11 +1,15 @@
 package my.cardholder.ui.cloud.login
 
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import my.cardholder.R
+import my.cardholder.data.model.CloudProvider
 import my.cardholder.databinding.FragmentCloudLoginBinding
 import my.cardholder.ui.base.BaseFragment
 import my.cardholder.util.ext.collectWhenStarted
+import my.cardholder.util.ext.setStartEndCompoundDrawables
 import my.cardholder.util.ext.updateVerticalPaddingAfterApplyingWindowInsets
 
 @AndroidEntryPoint
@@ -13,10 +17,10 @@ class CloudLoginFragment : BaseFragment<FragmentCloudLoginBinding>(
     FragmentCloudLoginBinding::inflate
 ) {
 
-    private val cloudSignInRequest = registerForActivityResult(
+    private val cloudLoginRequest = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { activityResult ->
-        viewModel.onGoogleCloudSignInRequestResult(activityResult)
+        viewModel.onLoginRequestResult(activityResult)
     }
 
     override val viewModel: CloudLoginViewModel by viewModels()
@@ -24,12 +28,56 @@ class CloudLoginFragment : BaseFragment<FragmentCloudLoginBinding>(
     override fun initViews() {
         with(binding) {
             root.updateVerticalPaddingAfterApplyingWindowInsets()
+            cloudLoginGoogleCloudCard.setOnClickListener {
+                viewModel.onGoogleCloudCardClicked()
+            }
+            cloudLoginYandexCloudCard.setOnClickListener {
+                viewModel.onYandexCloudCardClicked()
+            }
+            cloudLoginFab.setOnClickListener {
+                viewModel.onLoginFabClicked()
+            }
         }
     }
 
     override fun collectData() {
+        collectWhenStarted(viewModel.loginIntents) { intent ->
+            cloudLoginRequest.launch(intent)
+        }
         collectWhenStarted(viewModel.state) { state ->
-            viewModel.onCloudSignInRequestLaunched()
+            when (state) {
+                is CloudLoginState.Loading -> {
+                    enableOrDisableCards(false)
+                    binding.cloudLoginLoadingProgress.isVisible = true
+                }
+
+                is CloudLoginState.Selection -> {
+                    enableOrDisableCards(true)
+                    binding.cloudLoginLoadingProgress.isVisible = false
+
+                    val selectedProvider = state.selectedCloudProvider
+                    binding.cloudLoginGoogleCloudText.setStartEndCompoundDrawables(
+                        startDrawableResId = R.drawable.ic_cloud_google,
+                        endDrawableResId = if (selectedProvider == CloudProvider.GOOGLE) R.drawable.ic_done else null
+                    )
+                    binding.cloudLoginYandexCloudText.setStartEndCompoundDrawables(
+                        startDrawableResId = R.drawable.ic_cloud_yandex,
+                        endDrawableResId = if (selectedProvider == CloudProvider.YANDEX) R.drawable.ic_done else null
+                    )
+                }
+            }
+        }
+    }
+
+    private fun enableOrDisableCards(areEnabled: Boolean) {
+        val alphaValue = if (areEnabled) 1.0f else 0.5f
+        binding.cloudLoginGoogleCloudCard.apply {
+            alpha = alphaValue
+            isEnabled = areEnabled
+        }
+        binding.cloudLoginYandexCloudCard.apply {
+            alpha = alphaValue
+            isEnabled = areEnabled
         }
     }
 }
--- app/src/main/java/my/cardholder/ui/cloud/login/CloudLoginViewModel.kt
+++ app/src/main/java/my/cardholder/ui/cloud/login/CloudLoginViewModel.kt
@@ -1,24 +1,40 @@
 package my.cardholder.ui.cloud.login
 
+import android.content.Intent
 import androidx.activity.result.ActivityResult
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import my.cardholder.cloud.CloudSignInAssistant
 import my.cardholder.data.SettingsRepository
 import my.cardholder.data.model.CloudProvider
+import my.cardholder.di.Google
+import my.cardholder.di.Yandex
 import my.cardholder.ui.base.BaseViewModel
-import my.cardholder.util.GmsAvailabilityChecker
+import my.cardholder.util.Text
 import javax.inject.Inject
 
 @HiltViewModel
 class CloudLoginViewModel @Inject constructor(
-    private val gmsAvailabilityChecker: GmsAvailabilityChecker,
+    @Google private val googleCloudSignInAssistant: CloudSignInAssistant,
+    @Yandex private val yandexCloudSignInAssistant: CloudSignInAssistant,
     private val settingsRepository: SettingsRepository,
 ) : BaseViewModel() {
+
+    private companion object {
+        const val LOGIN_DELAY_MILLIS = 500L
+    }
+
+    private val loginIntentChannel = Channel<Intent>()
+    val loginIntents = loginIntentChannel.receiveAsFlow()
 
     private val _state = MutableStateFlow<CloudLoginState>(CloudLoginState.Loading)
     val state = _state.asStateFlow()
@@ -31,21 +47,46 @@ class CloudLoginViewModel @Inject constructor(
             .launchIn(viewModelScope)
     }
 
-    fun onGoogleCloudSignInRequestResult(activityResult: ActivityResult) {
+    fun onLoginRequestResult(activityResult: ActivityResult) {
+        viewModelScope.launch {
+            val provider = settingsRepository.cloudProvider.first()
+            when (provider) {
+                CloudProvider.GOOGLE ->
+                    googleCloudSignInAssistant.onSignInResult(activityResult)
+
+                CloudProvider.YANDEX ->
+                    yandexCloudSignInAssistant.onSignInResult(activityResult)
+            }.onSuccess { navigateUp() }
+                .onFailure {
+                    showToast(Text.Simple("ERROR: ${it.message}"))
+                    _state.value = CloudLoginState.Selection(selectedCloudProvider = provider)
+                }
+        }
     }
 
-    fun onCloudSignInRequestLaunched() {
-    }
-
-    fun onGoogleDriveButtonClicked() {
+    fun onGoogleCloudCardClicked() {
         viewModelScope.launch {
             settingsRepository.setCloudProvider(CloudProvider.GOOGLE)
         }
     }
 
-    fun onYandexDiskButtonChecked() {
+    fun onYandexCloudCardClicked() {
         viewModelScope.launch {
             settingsRepository.setCloudProvider(CloudProvider.YANDEX)
+        }
+    }
+
+    fun onLoginFabClicked() {
+        _state.value = CloudLoginState.Loading
+        viewModelScope.launch {
+            delay(LOGIN_DELAY_MILLIS)
+            when (settingsRepository.cloudProvider.first()) {
+                CloudProvider.GOOGLE ->
+                    loginIntentChannel.send(googleCloudSignInAssistant.signInIntent)
+
+                CloudProvider.YANDEX ->
+                    loginIntentChannel.send(yandexCloudSignInAssistant.signInIntent)
+            }
         }
     }
 }
