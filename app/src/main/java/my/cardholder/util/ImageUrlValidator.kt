679a944e71c3595b949daf86fca794a81010a2e9
kitfist0 <kitfist0@yandex.ru>
2025-09-21 12:35:37 +0300

    Use logo loader in image url validator

--- app/src/main/java/my/cardholder/util/ImageUrlValidator.kt
+++ app/src/main/java/my/cardholder/util/ImageUrlValidator.kt
@@ -1,20 +1,24 @@
 package my.cardholder.util
 
 import android.content.Context
-import coil.imageLoader
-import coil.request.ImageRequest
 import jakarta.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class ImageUrlValidator @Inject constructor(
     private val context: Context,
 ) {
-    suspend fun isValid(url: String): Boolean =
-        runCatching {
-            val request = ImageRequest.Builder(context)
-                .data(url)
-                .size(1, 1)
-                .allowHardware(false)
-                .build()
-            context.imageLoader.execute(request).drawable
-        }.getOrNull() != null
+    suspend fun isValid(url: String): Boolean = suspendCoroutine { continuation ->
+        LogoLoader(context).load(
+            imageUrl = url,
+            sizePx = 1,
+            cornerRadiusPx = 0,
+            onSuccess = {
+                continuation.resume(true)
+            },
+            onError = {
+                continuation.resume(false)
+            }
+        )
+    }
 }
