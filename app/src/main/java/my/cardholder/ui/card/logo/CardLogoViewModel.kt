19f97fdae6c8e90d65dc94e33bbbdd444e79445c
kitfist0 <kitfist0@yandex.ru>
2025-09-24 16:54:15 +0300

    Add sample logo links to build config

--- app/src/main/java/my/cardholder/ui/card/logo/CardLogoViewModel.kt
+++ app/src/main/java/my/cardholder/ui/card/logo/CardLogoViewModel.kt
@@ -3,20 +3,16 @@ package my.cardholder.ui.card.logo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import my.cardholder.BuildConfig
 import my.cardholder.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CardLogoViewModel @Inject constructor() : BaseViewModel() {
 
-    private companion object {
-        const val IMAGE_LINK = "https://play-lh.googleusercontent.com/CukdHWulsmo9nwmiLmJg9tViyUv2UfWV5KUMw-Emm-wnAmKfjItajKidbdRw0MR8i6o=w480-h960-rw"
-        const val WEBSITE_LINK = "https://play.google.com"
-    }
-
     private val defaultState = CardLogoState(
-        websiteIconLink = WEBSITE_LINK,
-        imageLogoLink = IMAGE_LINK,
+        websiteIconLink = BuildConfig.WEB_PAGE_STORE,
+        imageLogoLink = BuildConfig.APP_LOGO_LINK,
     )
     val state = MutableStateFlow(defaultState).asStateFlow()
 
