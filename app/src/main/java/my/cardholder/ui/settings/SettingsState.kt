78a5eac53fde7a4dc45f6282a37fa14507509108
kitfist0 <kitfist0@yandex.ru>
2025-10-09 21:36:01 +0300

    Add settings adapter

--- app/src/main/java/my/cardholder/ui/settings/SettingsState.kt
+++ app/src/main/java/my/cardholder/ui/settings/SettingsState.kt
@@ -2,6 +2,11 @@ package my.cardholder.ui.settings
 
 import my.cardholder.util.Text
 
+sealed class SettingsListItem {
+    data class Header(val cloudSyncEnabled: Boolean, val cloudName: String? = null) : SettingsListItem()
+    data class Item(val id: SettingsItem, val subtitle: String? = null) : SettingsListItem()
+}
+
 data class SettingsState(
     val nightModeEnabled: Boolean,
     val multiColumnListEnabled: Boolean,
