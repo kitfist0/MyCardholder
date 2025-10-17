ba0e26c384692dc7a1e637f5601bf35764347afc
kitfist0 <kitfist0@yandex.ru>
2025-10-17 11:55:55 +0300

    Add setting id enum

---
+++ app/src/main/java/my/cardholder/ui/settings/SettingId.kt
@@ -0,0 +1,30 @@
+package my.cardholder.ui.settings
+
+import my.cardholder.R
+
+enum class SettingId {
+    THEME,
+    COLUMNS,
+    CATEGORIES,
+    BACKUP,
+    COFFEE,
+    ABOUT
+}
+
+fun SettingId.getTitle(): String = when (this) {
+    SettingId.THEME -> "App theme"
+    SettingId.COLUMNS -> "Number of columns"
+    SettingId.CATEGORIES -> "Card categories"
+    SettingId.BACKUP -> "Import/export cards"
+    SettingId.COFFEE -> "Coffee for developers"
+    SettingId.ABOUT -> "About app"
+}
+
+fun SettingId.getImageRes(): Int = when (this) {
+    SettingId.THEME -> R.drawable.ic_dark_mode
+    SettingId.COLUMNS -> R.drawable.ic_list_single_column
+    SettingId.CATEGORIES -> R.drawable.ic_category
+    SettingId.BACKUP -> R.drawable.ic_import_export
+    SettingId.COFFEE -> R.drawable.ic_coffee
+    SettingId.ABOUT -> R.drawable.ic_info
+}
