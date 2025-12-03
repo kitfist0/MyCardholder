119afee8a7a00f9667ba3e9e979d845fa3ea1736
kitfist0 <kitfist0@yandex.ru>
2025-12-03 20:02:02 +0300

    Add payment repository

---
+++ app/src/main/java/my/cardholder/data/PaymentRepository.kt
@@ -0,0 +1,20 @@
+package my.cardholder.data
+
+import my.cardholder.data.model.PaymentOption
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PaymentRepository @Inject constructor(
+) {
+    private var _validPaymentOption: PaymentOption? = null
+    val validPaymentOption get() = _validPaymentOption
+
+    fun updateValidPaymentOption(paymentOption: PaymentOption) {
+        _validPaymentOption = paymentOption
+    }
+
+    fun deleteValidPaymentOption() {
+        _validPaymentOption = null
+    }
+}
