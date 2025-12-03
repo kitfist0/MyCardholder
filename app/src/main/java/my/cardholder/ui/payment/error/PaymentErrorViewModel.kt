81b2a245293293967e1fd426b870ede50ae72a0d
kitfist0 <kitfist0@yandex.ru>
2025-12-03 14:32:09 +0300

    Add error text argument to payment error fragment

--- app/src/main/java/my/cardholder/ui/payment/error/PaymentErrorViewModel.kt
+++ app/src/main/java/my/cardholder/ui/payment/error/PaymentErrorViewModel.kt
@@ -1,18 +1,22 @@
 package my.cardholder.ui.payment.error
 
+import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import my.cardholder.R
 import my.cardholder.ui.base.BaseViewModel
 import my.cardholder.util.Text
 import javax.inject.Inject
 
 @HiltViewModel
-class PaymentErrorViewModel @Inject constructor() : BaseViewModel() {
+class PaymentErrorViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel() {
     val state = MutableStateFlow(
         PaymentErrorState(
-            errorMessageText = Text.Resource(R.string.payment_error_unknown_error_message_text)
+            errorMessageText = Text.Simple(
+                text = PaymentErrorFragmentArgs.fromSavedStateHandle(savedStateHandle).errorText
+            )
         )
     ).asStateFlow()
 }
