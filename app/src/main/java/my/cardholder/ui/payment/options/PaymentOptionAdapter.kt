022864e179c47d5971beb41c9a4b4b80c2a1aea0
kitfist0 <kitfist0@yandex.ru>
2025-12-20 21:03:14 +0300

    Use spannable string for payment option titles

--- app/src/main/java/my/cardholder/ui/payment/options/PaymentOptionAdapter.kt
+++ app/src/main/java/my/cardholder/ui/payment/options/PaymentOptionAdapter.kt
@@ -1,10 +1,16 @@
 package my.cardholder.ui.payment.options
 
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.color.MaterialColors
 import my.cardholder.R
 import my.cardholder.data.model.PaymentOption
 import my.cardholder.data.model.PaymentOption.Companion.getTitleStringRes
@@ -38,12 +44,33 @@ class PaymentOptionAdapter(
 
         fun bind(paymentOptionState: PaymentOptionState) {
             binding.itemPaymentOptionText.apply {
-                text = context.getString(paymentOptionState.paymentOption.getTitleStringRes())
-                    .format(paymentOptionState.costOfPayment)
+                text = getSpannableTitle(paymentOptionState)
                 setStartEndCompoundDrawables(
                     endDrawableResId = if (paymentOptionState.isSelected) R.drawable.ic_done else null
                 )
             }
+        }
+
+        private fun View.getSpannableTitle(paymentOptionState: PaymentOptionState): SpannableString {
+            val price = paymentOptionState.costOfPayment
+            val titleWithPrice =
+                context.getString(paymentOptionState.paymentOption.getTitleStringRes())
+                    .format(price)
+            val spannableString = SpannableString(titleWithPrice)
+            val start = titleWithPrice.indexOf(price)
+            val end = titleWithPrice.length
+            val color = MaterialColors.getColor(
+                this,
+                com.google.android.material.R.attr.colorTertiary,
+                Color.LTGRAY
+            )
+            spannableString.setSpan(
+                ForegroundColorSpan(color),
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            return spannableString
         }
     }
 
