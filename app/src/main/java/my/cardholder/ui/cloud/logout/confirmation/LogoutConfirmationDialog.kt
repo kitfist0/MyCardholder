a9a8b19a699aed7a99ff23871359343d08e48b14
kitfist0 <kitfist0@yandex.ru>
2026-01-03 22:06:05 +0300

    Use spannable string in logout confirmation dialog

--- app/src/main/java/my/cardholder/ui/cloud/logout/confirmation/LogoutConfirmationDialog.kt
+++ app/src/main/java/my/cardholder/ui/cloud/logout/confirmation/LogoutConfirmationDialog.kt
@@ -1,12 +1,16 @@
 package my.cardholder.ui.cloud.logout.confirmation
 
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import my.cardholder.R
 import my.cardholder.databinding.DialogLogoutConfirmationBinding
 import my.cardholder.ui.base.BaseDialogFragment
-import my.cardholder.ui.cloud.logout.confirmation.LogoutConfirmationState.Companion.getTitleText
 import my.cardholder.util.ext.collectWhenStarted
-import my.cardholder.util.ext.textToString
 
 @AndroidEntryPoint
 class LogoutConfirmationDialog : BaseDialogFragment<DialogLogoutConfirmationBinding>(
@@ -23,7 +27,23 @@ class LogoutConfirmationDialog : BaseDialogFragment<DialogLogoutConfirmationBind
 
     override fun collectData() {
         collectWhenStarted(viewModel.state) { state ->
-            binding.logoutConfirmationTitleText.text = textToString(state.getTitleText())
+            binding.logoutConfirmationTitleText.setSpannableText(state.accountName)
         }
+    }
+
+    private fun TextView.setSpannableText(accountName: String) {
+        val textWithAccountName =
+            context.getString(R.string.cloud_logout_confirmation_title_text)
+                .format(accountName)
+        val spannableString = SpannableString(textWithAccountName)
+        val start = textWithAccountName.indexOf(accountName)
+        val end = start + accountName.length
+        spannableString.setSpan(
+            StyleSpan(Typeface.BOLD),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        text = spannableString
     }
 }
